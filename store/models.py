45576258d5917825c026a60b01a116e0ab731abe
Gour S Patar <goursundarpatar2@gmail.com>
2026-08-27 21:57:09 +0530

    Created the relationship bet category and products

--- store/models.py
+++ store/models.py
@@ -11,5 +11,11 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    category = models.ForeignKey(
+    Category,
+    on_delete=models.CASCADE,
+    related_name='products'
+)
 
 # Create your models here.
