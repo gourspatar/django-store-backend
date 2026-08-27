5f7191ade94c8a618f7069a987d2e38da1a93198
Gour S Patar <goursundarpatar2@gmail.com>
2026-08-27 21:48:07 +0530

    Created the Product model

--- store/models.py
+++ store/models.py
@@ -7,4 +7,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
 # Create your models here.
