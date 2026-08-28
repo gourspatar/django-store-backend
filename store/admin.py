515abe8783484c31e88a31b8b774b419b7d357e8
Gour S Patar <goursundarpatar2@gmail.com>
2026-08-28 15:06:10 +0530

    Build store models and configure Django admin

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Category, Product
 
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "category")
+    search_fields = ("name", "description")
+    list_filter = ("category",)
