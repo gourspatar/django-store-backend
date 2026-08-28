384e47d01da3520be61a1658f2b8217ac0275d95
Gour S Patar <goursundarpatar2@gmail.com>
2026-08-28 21:42:01 +0530

    Add product serializer and product API view

--- store/views.py
+++ store/views.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category , Product
+from .serializers import CategorySerializer , ProductSerializer
 
 
 class CategoryListCreateView(APIView):
