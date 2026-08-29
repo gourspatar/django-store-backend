8946c6f688db6f6ff6f5e6def94703c090402dde
Gour S Patar <goursundarpatar2@gmail.com>
2026-08-29 11:39:30 +0530

    Build product and category APIs with CRUD

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from .views import CategoryListCreateView, CategoryDetailView, ProductListCreateView
+from .views import CategoryListCreateView, CategoryDetailView, ProductListCreateView, ProductDetailView
 
 urlpatterns = [
-    path("categories/", CategoryListCreateView.as_view()),
-    path("products/", ProductListCreateView.as_view()),
-    path("categories/<int:pk>/", CategoryDetailView.as_view()
+    path(
+        "categories/",
+        CategoryListCreateView.as_view()
+    ),
+    path(
+        "categories/<int:pk>/",
+        CategoryDetailView.as_view()
+    ),
+    path(
+        "products/",
+        ProductListCreateView.as_view()
+    ),
+    path(
+    "products/<int:pk>/",
+    ProductDetailView.as_view()
 ),
 ]
--- store/views.py
+++ store/views.py
@@ -54,6 +54,39 @@ class ProductListCreateView(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
+class ProductDetailView(APIView):
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+
+        serializer = ProductSerializer(
+            product,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+
 class CategoryDetailView(APIView):
 
     def get(self, request, pk):
