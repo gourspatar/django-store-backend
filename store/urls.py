aaec183fb4efafde0d3d2026bb802c6861d17f6e
Gour S Patar <goursundarpatar2@gmail.com>
2026-08-29 09:04:35 +0530

    Add category detail API endpoint

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CategoryListCreateView, ProductListCreateView
+from .views import CategoryListCreateView, CategoryDetailView, ProductListCreateView
 
 urlpatterns = [
     path("categories/", CategoryListCreateView.as_view()),
     path("products/", ProductListCreateView.as_view()),
+    path("categories/<int:pk>/", CategoryDetailView.as_view()
+),
 ]
--- store/views.py
+++ store/views.py
@@ -52,3 +52,35 @@ class ProductListCreateView(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+
+class CategoryDetailView(APIView):
+
+    def get(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        serializer = CategorySerializer(category)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        serializer = CategorySerializer(
+            category,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        category.delete()
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
